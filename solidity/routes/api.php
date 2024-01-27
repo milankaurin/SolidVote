455af71c621022ac22abecf4a30cc75d73962fba
Milan Kaurin <milan.kaurin2@gmail.com>
2024-01-27 13:12:04 +0100

    kreiranje osnovnih ruta i kontolera
    kreiranje osnovnih ruta i kontolera

--- solidity/routes/api.php
+++ solidity/routes/api.php
@@ -14,6 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// API rute za korisnike
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
+Route::post('/logout', 'AuthController@logout');
+
+// API rute koje zahtevaju autentifikaciju
+Route::middleware('auth:sanctum')->group(function () {
+    // Rute za Contract model
+    Route::apiResource('/contracts', 'ContractController');
+
+    // Rute za Transaction model
+    Route::apiResource('/transactions', 'TransactionController');
+    
+    
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
