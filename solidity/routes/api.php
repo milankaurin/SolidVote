0d6e23f28aeee508d123d09b1c77f49145d47e90
Milan Kaurin <milan.kaurin2@gmail.com>
2024-01-27 18:18:57 +0100

    gotove api za sada

--- solidity/routes/api.php
+++ solidity/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ContractController;
+use App\Http\Controllers\TransactionController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,16 +20,47 @@ use App\Http\Controllers\AuthController;
 
 // API rute za korisnike
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', 'AuthController@login');
-Route::post('/logout', 'AuthController@logout');
+// Logovanje korisnika
+Route::post('/login', [AuthController::class, 'login']);
+
+// Odjavljivanje korisnika
+Route::post('/logout', [AuthController::class, 'logout']);
+
+// Dohvatanje svih transakcija
+Route::get('/transactions', [TransactionController::class, 'index']);
+
+// Kreiranje nove transakcije
+Route::post('/transactions', [TransactionController::class, 'store']);
+
+// Dohvatanje pojedinačne transakcije
+Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+
+// Ažuriranje transakcije
+Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+
+// Brisanje transakcije
+Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
+
+
+// Dohvatanje svih ugovora
+Route::get('/contracts', [ContractController::class, 'index']);
+
+// Kreiranje novog ugovora
+Route::post('/contracts', [ContractController::class, 'store']);
+
+// Dohvatanje pojedinačnog ugovora
+Route::get('/contracts/{id}', [ContractController::class, 'show']);
+
+// Ažuriranje ugovora
+Route::put('/contracts/{id}', [ContractController::class, 'update']);
+
+// Brisanje ugovora
+Route::delete('/contracts/{id}', [ContractController::class, 'destroy']);
+
 
 // API rute koje zahtevaju autentifikaciju
 Route::middleware('auth:sanctum')->group(function () {
-    // Rute za Contract model
-    Route::apiResource('/contracts', 'ContractController');
-
-    // Rute za Transaction model
-    Route::apiResource('/transactions', 'TransactionController');
+   
     
     
 });
