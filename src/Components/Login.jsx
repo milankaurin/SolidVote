63130e289644ae21aea6db79098208150215fd1a
Milan Kaurin <milan.kaurin2@gmail.com>
2024-02-05 12:57:39 +0100

    login pozadina

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 const Login = (props) => {
+
+    const backgroundStyle = {
+        backgroundImage: `url('/images/adminBackground.jpg')`,
+        backgroundSize: 'cover', // Pokriva celu pozadinu
+        backgroundPosition: 'center', // Centrira sliku
+        height: '100vh', // Visina pozadine
+        width: '100vw' // Širina pozadine
+    };
     return (
-        <div className="login-container">
+        <div style={backgroundStyle} className="login-container">
             <h1 className="welcome-message">Dobrodošli u decentralizovanu veb aplikaciju za glasanje!</h1>
             <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
         </div>
