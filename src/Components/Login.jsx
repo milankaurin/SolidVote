bf8a633b35024a1ee372d9b79733dd024f4cede3
Milan Kaurin <milan.kaurin2@gmail.com>
2024-03-18 23:33:38 +0100

    Nista

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -21,7 +21,25 @@ const Login = (props) => {
   fontWeight: '530',
   fontFamily: "'Basel', sans-serif",
 }}>
-  Welcome to the SolidVote! Decentralized, secure, transparent.
+ 
+ <Typography
+  sx={{
+    fontSize: '2rem', // Large text size
+    fontWeight: 'bold', // Bold text for impact
+    color: '#ff007a', // Uniswap's signature dark pink color
+    textShadow: '0 2px 2px rgba(0, 0, 0, 0.5)', // A subtle shadow for depth
+    fontFamily: 'Helvetica, Arial, sans-serif', // A modern, sans-serif font
+    textAlign: 'center', // Center-aligned for emphasis
+    backgroundColor: '#1a1a1a', // A dark background for contrast
+    padding: '20px', // Padding for space around the text
+    borderRadius: '4px', // Slightly rounded corners for a polished look
+    border: '1px solid #ff007a', // Border in Uniswap pink for definition
+  }}
+>
+ 
+Introducing a secure, anonymous blockchain voting app: the pinnacle of privacy and trust in digital democracy, where every vote is a fortress of freedom.
+</Typography>
+
 </Typography>
            
             <Button variant="contained" onClick= {props.connectWallet}  sx={{
